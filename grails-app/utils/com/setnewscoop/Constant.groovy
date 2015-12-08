49431a33cf178880a85e81ef9bc31442b014ef26
kanpot2002 <kanpot2002@hotmail.com>
2015-12-08 12:46:30 +0700

    Config and Constant

--- grails-app/utils/com/setnewscoop/Constant.groovy
+++ grails-app/utils/com/setnewscoop/Constant.groovy
@@ -1,8 +1,11 @@
+package com.setnewscoop
 /**
  * Created by Lenovo on 21/11/2558.
  */
 class Constant {
     public static int numGuarantee = 2;
     public static int maxGuaranteeBalance = 150000;
+
+    public static String titleMrTh;
 
 }
