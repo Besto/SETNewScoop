a7aa58b915a5a52e6dca1d5f233c4e7c8fd3893a
Oz <ozsavior@gmail.com>
2015-12-08 14:37:38 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	grails-app/utils/com/setnewscoop/Constant.groovy

--- grails-app/utils/com/setnewscoop/Constant.groovy
+++ grails-app/utils/com/setnewscoop/Constant.groovy
@@ -5,4 +5,25 @@ class Constant {
     public static int numGuarantee = 2;
     public static int maxGuaranteeBalance = 150000;
 
+
+    public  static ArrayList<String> COMPANY = new ArrayList<String>() {{
+        add("SET");
+        add("FKH");
+        add("COO");
+    }};
+
+    public  static ArrayList<String> MEMBER_STATUS = new ArrayList<String>() {{
+        add("โสด");
+        add("สมรส");
+        add("สมรสไม่จดทะเบียน");
+    }};
+
+    public  static ArrayList<String> MEMBER_TITLE = new ArrayList<String>() {{
+        add("นาย");
+        add("น.ส.");
+        add("นาง");
+        add("อื่นๆ");
+    }};
+
+
 }
