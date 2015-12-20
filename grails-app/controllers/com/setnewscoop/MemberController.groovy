41818d8a71f9951e5f6a800b621787d0a79390ae
Oz <ozsavior@gmail.com>
2015-12-20 19:38:05 +0700

    Oz feature add Member edit Member

--- grails-app/controllers/com/setnewscoop/MemberController.groovy
+++ grails-app/controllers/com/setnewscoop/MemberController.groovy
@@ -2,6 +2,7 @@ package com.setnewscoop
 
 import java.text.DateFormat
 import java.text.SimpleDateFormat
+import java.text.DecimalFormat
 
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
@@ -9,7 +10,7 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class MemberController {
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+    static allowedMethods = [create: ["POST","GET"], save: "POST", update: "POST", delete: "POST"]
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
@@ -44,16 +45,8 @@ class MemberController {
         Member memberInstance = new Member();
         bindData(memberInstance,params);
 
-//        DateFormat df = new SimpleDateFormat("dd/MM/yyy",Locale.US);
-//        String dStart = params.d_start;
-//        Date d_start = df.parse(dStart);
-//        memberInstance.d_start = d_start;
-//
-//        String dMember = params.d_member_string;
-//        Date _dMember = df.parse(dMember);
-//        memberInstance.d_member = _dMember;
-
-        println("memberInstance.d_member : " + memberInstance.d_member);
+        String shared = params.m_share;
+        memberInstance.m_share = Double.parseDouble(shared.replaceAll(",", ""));
 
         if(!memberInstance.save(flush: true ,failOnError: true)){
             render(view:  "create" ,model: [memberInstance:memberInstance,memberId: memberId])
@@ -64,31 +57,93 @@ class MemberController {
 
     }
 
-    def edit(Member memberInstance) {
-        respond memberInstance
+    def edit() {
+        String memberId = params.id_member;
+        Member memberInstance = Member.findById(memberId);
+
+        render( view: "edit", model: [memberInstance: memberInstance, memberId:memberInstance.id, type: 'edit'])
+
+    }
+
+
+    def editMember(){
+
+        int idMember = -1;
+        try {
+            idMember = Integer.parseInt(params.id_member);
+        }
+        catch (Exception e){
+            //e.printStackTrace();
+        }
+
+        int idEmployee = -1;
+        try {
+            idEmployee = Integer.parseInt(params.id_employee);
+        }
+        catch (Exception e){
+            //e.printStackTrace();
+        }
+
+        String firstName = params.first_name;
+        String lastName = params.last_name;
+
+        println("idMember : "+idMember);
+        println("idEmployee : "+idEmployee);
+        println("firstName : "+firstName);
+        println("lastName : "+lastName);
+
+        if(idMember!=-1 || idEmployee!=-1 || firstName!=null || lastName!=null) {
+            def c = Member.createCriteria()
+            def results = c.list {
+                if (idMember != -1) {
+                    eq("id", idMember)
+                }
+                if (idEmployee != -1) {
+                    eq("i_employee", idEmployee)
+                }
+                if (!firstName.equals("")) {
+                    eq("n_first", firstName)
+                }
+                if (!lastName.equals("")) {
+                    eq("n_last", lastName)
+                }
+                isNull("d_expired")
+            }
+
+            String errCode = "";
+            int resultCount = results.size();
+            println("result : " + resultCount);
+
+            if (resultCount == 0) {
+                errCode = "1001" //not found
+                render view: "searchMember", model: [type: "edit", errCode: errCode];
+            }
+
+            if(resultCount == 1){
+                Member memberInstance = (Member) results.get(0);
+                render( view: "edit", model: [memberInstance: memberInstance, memberId:memberInstance.id, type: 'edit'])
+            }else if (resultCount > 1){
+                render view: "searchMember", model: [type: "edit", members: results];
+            }
+
+        }else{
+            render view: "searchMember",model: [type:"edit"]
+
+        }
     }
 
     @Transactional
-    def update(Member memberInstance) {
-        if (memberInstance == null) {
-            notFound()
-            return
+    def update() {
+        Member memberInstance = Member.findById(params.memberId);
+
+        bindData(memberInstance,params);
+
+        if(!memberInstance.save(flush: true ,failOnError: true)){
+            render( view: "edit", model: [memberInstance: memberInstance, memberId:memberInstance.id, type: 'edit'])
         }
 
-        if (memberInstance.hasErrors()) {
-            respond memberInstance.errors, view:'edit'
-            return
-        }
-
-        memberInstance.save flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Member.label', default: 'Member'), memberInstance.id])
-                redirect memberInstance
-            }
-            '*'{ respond memberInstance, [status: OK] }
-        }
+        println(">>>update success");
+        render view: "searchMember", model: [type: "edit", errCode: "1000"];
     }
 
     @Transactional
