a94b9e0ff4af4df15a9995a757d9a36d4ba6ea4a
Oz <ozsavior@gmail.com>
2016-01-12 01:29:36 +0700

    Oz Add Enum
    ---
    add UI Change shared and Model

--- grails-app/controllers/com/setnewscoop/MemberController.groovy
+++ grails-app/controllers/com/setnewscoop/MemberController.groovy
@@ -132,6 +132,146 @@ class MemberController {
         }
     }
 
+    def resignMember(){
+
+        int idMember = -1;
+        try {
+            idMember = Integer.parseInt(params.id_member);
+        }
+        catch (Exception e){
+            //e.printStackTrace();
+        }
+
+        int idEmployee = -1;
+        try {
+            idEmployee = Integer.parseInt(params.id_employee);
+        }
+        catch (Exception e){
+            //e.printStackTrace();
+        }
+
+        String firstName = params.first_name;
+        String lastName = params.last_name;
+
+        println("idMember : "+idMember);
+        println("idEmployee : "+idEmployee);
+        println("firstName : "+firstName);
+        println("lastName : "+lastName);
+
+        if(idMember!=-1 || idEmployee!=-1 || firstName!=null || lastName!=null) {
+            def c = Member.createCriteria()
+            def results = c.list {
+                if (idMember != -1) {
+                    eq("id", idMember)
+                }
+                if (idEmployee != -1) {
+                    eq("i_employee", idEmployee)
+                }
+                if (!firstName.equals("")) {
+                    eq("n_first", firstName)
+                }
+                if (!lastName.equals("")) {
+                    eq("n_last", lastName)
+                }
+                isNull("d_expired")
+            }
+
+            String errCode = "";
+            int resultCount = results.size();
+            println("result : " + resultCount);
+
+            if (resultCount == 0) {
+                errCode = "1001" //not found
+                render view: "searchMember", model: [type: "edit", errCode: errCode];
+            }
+
+            if(resultCount == 1){
+                Member memberInstance = (Member) results.get(0);
+                render( view: "edit", model: [memberInstance: memberInstance, memberId:memberInstance.id, type: 'edit'])
+            }else if (resultCount > 1){
+                render view: "searchMember", model: [type: "edit", members: results];
+            }
+
+        }else{
+            render view: "searchMember",model: [type:"resign"]
+
+        }
+    }
+
+    def changeSharedMember(){
+
+        int idMember = -1;
+        try {
+            idMember = Integer.parseInt(params.id_member);
+        }
+        catch (Exception e){
+            //e.printStackTrace();
+        }
+
+        int idEmployee = -1;
+        try {
+            idEmployee = Integer.parseInt(params.id_employee);
+        }
+        catch (Exception e){
+            //e.printStackTrace();
+        }
+
+        String firstName = params.first_name;
+        String lastName = params.last_name;
+
+        println("idMember : "+idMember);
+        println("idEmployee : "+idEmployee);
+        println("firstName : "+firstName);
+        println("lastName : "+lastName);
+
+        if(idMember!=-1 || idEmployee!=-1 || firstName!=null || lastName!=null) {
+            def c = Member.createCriteria()
+            def results = c.list {
+                if (idMember != -1) {
+                    eq("id", idMember)
+                }
+                if (idEmployee != -1) {
+                    eq("i_employee", idEmployee)
+                }
+                if (!firstName.equals("")) {
+                    eq("n_first", firstName)
+                }
+                if (!lastName.equals("")) {
+                    eq("n_last", lastName)
+                }
+                isNull("d_expired")
+            }
+
+            String errCode = "";
+            int resultCount = results.size();
+            println("result : " + resultCount);
+
+            if (resultCount == 0) {
+                errCode = "1001" //not found
+                render view: "searchMember", model: [type: "changeShared", errCode: errCode];
+            }
+
+            if(resultCount == 1){
+                Member memberInstance = (Member) results.get(0);
+                render( view: "changeShared", model: [memberInstance: memberInstance])
+            }else if (resultCount > 1){
+                render view: "searchMember", model: [type: "changeShared", members: results];
+            }
+
+        }else{
+            render view: "searchMember",model: [type:"changeShared"]
+
+        }
+    }
+
+    def changeShared() {
+        String memberId = params.id_member;
+        Member memberInstance = Member.findById(memberId);
+
+        render( view: "changeShared", model: [memberInstance: memberInstance])
+
+    }
+
     @Transactional
     def update() {
         Member memberInstance = Member.findById(params.memberId);
